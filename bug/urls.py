437d9c62e27d055d0d0926e00223725099b27be2
oghenekevbe <oghenekevbe.egume@gmail.com>
2023-10-03 15:50:23 +0100

    bug list and bug detail

--- bug/urls.py
+++ bug/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from .views import *
 
 
 urlpatterns = [
-    
+    path("bug_list/", BugListView.as_view(), name="bug_list"),
+    path("bug_detail/<int:pk>/", BugDetailView.as_view(), name="bug_detail")
 ]
--- bug/views.py
+++ bug/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.views.generic import CreateView, DetailView,ListView
+from .models import Bug
 
 # Create your views here.
+
+
+
+class BugListView(ListView):
+    model = Bug
+    template_name = 'bug_list.html'
+    context_object_name = 'bugs'
+
+
+class BugDetailView(DetailView):
+    model = Bug
+    template_name = 'bug_detail.html'
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["bug"] = self.get_object()
+        return context
+    
+
