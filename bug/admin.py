e95ac1dbae2c848fc11180159b73d76a5d295748
oghenekevbe <oghenekevbe.egume@gmail.com>
2023-10-03 14:01:42 +0100

    added bug model

--- bug/admin.py
+++ bug/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Bug
 # Register your models here.
+
+@admin.register(Bug)
+class BugAdmin(admin.ModelAdmin):
+    '''Admin View for '''
+
+    list_display = ('bug_type', 'status', 'report_date')
+    list_filter = ('report_date',)
--- bug/models.py
+++ bug/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+
+class Bug(models.Model):
+    
+    STATUSES =[
+            ('new', 'New'),
+            ('assigned', 'Assigned'),
+            ('in_progress', 'In Progress'),
+            ('resolved', 'Resolved'),
+            ('closed', 'Closed'),
+            ('reopened', 'Reopened'),
+            ('duplicate', 'Duplicate'),
+            ('invalid', 'Invalid'),
+            ('wont_fix', "Won't Fix"),
+    ]
+
+    description = models.TextField()
+    bug_type = models.CharField(max_length=225)
+    report_date = models.DateTimeField(default=datetime.now)
+    status = models.CharField(max_length=50, unique=True, choices=STATUSES)
+
+
+    def __str__(self):
+        return self.bug_type
+
